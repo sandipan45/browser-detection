5a0ae490ea989986c3faa75fc51a4b7fec9cab0d
sandipan45 <sandipan45@gmail.com>
2020-06-16 16:31:06 +0530

    updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,6 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import * as rdd from 'react-device-detect'
 
-const si = require('systeminformation');
- 
-// promises style - new since version 3
-si.cpu()
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
-
 
 const getRes = require('get-res');
 //console.log(getRes()); 

1d8745c73c0549210926ee8f4cce41f9a55054ab
sandipan45 <sandipan45@gmail.com>
2020-06-18 15:58:58 +0530

    updated

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -76,7 +76,7 @@ export const onInitialClientRender = () => {
 		document.getElementById("pixelDensity").innerHTML = pixelDensity(widthView, heightView, diag);
 
 		var enumeratorPromise = navigator.mediaDevices.enumerateDevices();
-		//
+		console.log(enumeratorPromise);
 		
 		
 		var connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection;
@@ -86,7 +86,7 @@ export const onInitialClientRender = () => {
 		document.getElementById("networkconnectionType").innerHTML = type;
 		document.getElementById("networkDownlink").innerHTML = downlink;
 		document.getElementById("networkrtt").innerHTML = netrtt;
-		console.log(connection);
+		
 		
 
 

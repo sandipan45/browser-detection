44eee552ae93338b3efc68965ec9ee760f38813d
sandipan45 <sandipan45@gmail.com>
2020-06-26 14:13:34 +0530

    Next button added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import * as rdd from 'react-device-detect'
